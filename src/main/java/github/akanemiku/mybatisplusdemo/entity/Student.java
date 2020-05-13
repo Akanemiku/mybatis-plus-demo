df031275bd6a986d16eaab8d65ae7679cabe52f7
Akanemiku <806394913@qq.com>
2020-05-13 17:49:31 +0800

    fix CRUD bugs

--- src/main/java/github/akanemiku/mybatisplusdemo/entity/Student.java
+++ src/main/java/github/akanemiku/mybatisplusdemo/entity/Student.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @Data
 public class Student implements Serializable {
@@ -16,4 +17,6 @@ public class Student implements Serializable {
     private String name;
 
     private String college;
+
+    private Date time;
 }
